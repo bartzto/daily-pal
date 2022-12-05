8d6246daeb41b976f139c435ef7357531769f3b2
Tobias Bartz <bartz.tobias@icloud.com>
2022-12-05 19:23:42 +0100

    Added new Views

---
+++ dailypal/Models/Main/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  dailypal
+//
+//  Created by Tobias Bartz on 05.12.22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
---
+++ dailypal/Models/Main/SettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsView.swift
+//  dailypal
+//
+//  Created by Tobias Bartz on 05.12.22.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
