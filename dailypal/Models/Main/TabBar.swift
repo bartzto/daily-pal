24367dc4311f0b027d259e93a1ea27a6c8074ab7
Tobias Bartz <bartz.tobias@icloud.com>
2022-12-05 19:32:33 +0100

    Added TabBar

---
+++ dailypal/Models/Main/TabBar.swift
@@ -0,0 +1,30 @@
+//
+//  TabBar.swift
+//  dailypal
+//
+//  Created by Tobias Bartz on 05.12.22.
+//
+
+import SwiftUI
+
+struct TabBar: View {
+    var body: some View {
+        TabView {
+            HomeView()
+                .tabItem {
+                    Label("Home", systemImage: "house")
+                }
+            
+            SettingsView()
+                .tabItem {
+                    Label("Settings", systemImage: "gear")
+                }
+        }
+    }
+}
+
+struct TabBar_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBar()
+    }
+}
