1a080760bab5c20200b3ad5ddc3ac12c365968c5
Tobias Bartz <bartz.tobias@icloud.com>
2022-12-05 19:10:01 +0100

    Initial Commit

---
+++ dailypal/dailypalApp.swift
@@ -0,0 +1,17 @@
+//
+//  dailypalApp.swift
+//  dailypal
+//
+//  Created by Tobias Bartz on 05.12.22.
+//
+
+import SwiftUI
+
+@main
+struct dailypalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
